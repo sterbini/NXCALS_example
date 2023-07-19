86ae9a6a976ce5aefc16b8b0a1164b2b5a25e284
Guido Sterbini <guido.sterbini@cern.ch>
2023-07-19 16:57:40 +0200

    Status

---
+++ make_miniforge.sh
@@ -0,0 +1,8 @@
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
+source miniforge/bin/activate
+python3 -m venv ./venv
+source ./venv/bin/activate
+python -m pip install -r requirements.txt
+python -m pip install git+https://gitlab.cern.ch/acc-co/devops/python/acc-py-pip-config.git
+python -m pip install nxcals
---
+++ test.py
@@ -0,0 +1,9 @@
+# %%
+from nxcals.spark_session_builder import get_or_create
+from nxcals.api.extraction.data.builders import DataQuery
+spark = get_or_create("My_APP")
+
+df = DataQuery.builder(spark).entities().system('CMW') \
+    .keyValuesEq({'device': 'LHC.LUMISERVER', 'property': 'CrossingAngleIP1'}) \
+    .timeWindow('2022-04-22 00:00:00.000', '2022-04-23 00:00:00.000') \
+    .build()
